6b1f95bf2297c797945bfe504d5d22e270ca82f7
glontinika <glontinika@gmail.com>
2025-01-28 17:07:44 +0400

    Third V1

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,11 +1,46 @@
 package converter
 
+import kotlin.system.exitProcess
+
 fun main() {
-    println("""
-        145 centimeters is 1.45 meters
-        2 miles is 3.2187 kilometers
-        5.5 inches is 139.7 millimeters
-        12 degrees Celsius is 53.6 degrees Fahrenheit
-        3 pounds is 1.360776 kilograms
-    """.trimIndent())
+    startConvertor()
+}
+
+fun startConvertor(){
+    val (num, um) = askForInput()
+    val unitOfMeasure = getUnitOfMeasure(num, um)
+    printOutput (unitOfMeasure, num)
+}
+
+enum class UnitOfMeasure(val single: String, val plural: String, val conv: Double, val keywords: List<String>){
+    Meter("meter", "meters", 1.0, listOf("m","meter","meters")),
+    Kilometer("kilometer", "kilometers", 1.0, listOf("km","kilometer","kilometers")),
+    Centimeter("centimeter", "centimeters", 0.01, listOf("cm","centimeter","centimeters")),
+    Millimeter("millimeter", "millimeters", 0.001, listOf("mm","millimeter","millimeters")),
+    Mile("mile", "miles", 1609.35, listOf("mi","mile","miles")),
+    Yard("yard", "yards", 0.9144, listOf("yd","yard","yards")),
+    Foot("foot", "feet", 0.3048, listOf("ft","foot","feet")),
+    Inch("inch", "inches", 0.0254, listOf("in","inch","inches"))
+}
+
+fun askForInput(): Pair<Double, String> {
+    println("Enter a number and a measure: ")
+    val input = readln()
+    val (nu, um) = input.split(" ")
+    val num = nu.toDouble()
+    return Pair(num, um)
+}
+
+fun getUnitOfMeasure(num: Double, um: String): UnitOfMeasure {
+    lateinit var theOutput: UnitOfMeasure
+    for (unit in UnitOfMeasure.entries){
+        if (um in unit.keywords) theOutput = unit
+    }
+    return theOutput // aq dasamatebelia wrong input-is erori
+}
+
+fun printOutput(unitOfMeasure: UnitOfMeasure, num: Double){
+    val unName = if (num != 1.0) unitOfMeasure.plural else unitOfMeasure.single
+    val outputNum = num * unitOfMeasure.conv
+    println("$num $unName is $outputNum meter${if (outputNum!=1.0) "s" else ""}")
 }
