49bbbb2aa66d3bc897b0b23ddc8cad19a7bc836d
glontinika <glontinika@gmail.com>
2025-01-29 01:31:51 +0400

    Third V1

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -3,44 +3,57 @@ package converter
 import kotlin.system.exitProcess
 
 fun main() {
-    startConvertor()
+    val convertor = Convertor()
+    convertor.start()
 }
 
-fun startConvertor(){
-    val (num, um) = askForInput()
-    val unitOfMeasure = getUnitOfMeasure(num, um)
-    printOutput (unitOfMeasure, num)
+enum class UnitOfMeasure(val single: String, val plural: String, val conv: Double, val keywords: List<String>) {
+    Meter("meter", "meters", 1.0, listOf("m", "meter", "meters")),
+    Kilometer("kilometer", "kilometers", 1000.0, listOf("km", "kilometer", "kilometers")),
+    Centimeter("centimeter", "centimeters", 0.01, listOf("cm", "centimeter", "centimeters")),
+    Millimeter("millimeter", "millimeters", 0.001, listOf("mm", "millimeter", "millimeters")),
+    Mile("mile", "miles", 1609.35, listOf("mi", "mile", "miles")),
+    Yard("yard", "yards", 0.9144, listOf("yd", "yard", "yards")),
+    Foot("foot", "feet", 0.3048, listOf("ft", "foot", "feet")),
+    Inch("inch", "inches", 0.0254, listOf("in", "inch", "inches"))
 }
 
-enum class UnitOfMeasure(val single: String, val plural: String, val conv: Double, val keywords: List<String>){
-    Meter("meter", "meters", 1.0, listOf("m","meter","meters")),
-    Kilometer("kilometer", "kilometers", 1.0, listOf("km","kilometer","kilometers")),
-    Centimeter("centimeter", "centimeters", 0.01, listOf("cm","centimeter","centimeters")),
-    Millimeter("millimeter", "millimeters", 0.001, listOf("mm","millimeter","millimeters")),
-    Mile("mile", "miles", 1609.35, listOf("mi","mile","miles")),
-    Yard("yard", "yards", 0.9144, listOf("yd","yard","yards")),
-    Foot("foot", "feet", 0.3048, listOf("ft","foot","feet")),
-    Inch("inch", "inches", 0.0254, listOf("in","inch","inches"))
-}
+class Convertor {
+    private lateinit var umInput: String
+    private var numberDouble: Double = 0.0
+    lateinit var unitOfMeasure: UnitOfMeasure
 
-fun askForInput(): Pair<Double, String> {
-    println("Enter a number and a measure: ")
-    val input = readln()
-    val (nu, um) = input.split(" ")
-    val num = nu.toDouble()
-    return Pair(num, um)
-}
-
-fun getUnitOfMeasure(num: Double, um: String): UnitOfMeasure {
-    lateinit var theOutput: UnitOfMeasure
-    for (unit in UnitOfMeasure.entries){
-        if (um in unit.keywords) theOutput = unit
+    fun start(){
+        askForInput()
+        getUnitOfMeasure()
+        printOutput()
     }
-    return theOutput // aq dasamatebelia wrong input-is erori
+
+    private fun askForInput() {
+        println("Enter a number and a measure of length: ")
+        val input = readln()
+        val (numberInput, umTxt) = input.split(" ")
+        umInput = umTxt
+        numberDouble = numberInput.toDouble()
+    }
+
+    private fun getUnitOfMeasure() {
+        for (unit in UnitOfMeasure.entries) {
+            if (umInput.lowercase() in unit.keywords) {
+                unitOfMeasure = unit
+                break
+            }
+        }
+        if (!::unitOfMeasure.isInitialized) {
+            println("Wrong input. Unknown unit $umInput")
+            exitProcess(0)
+        }
+    }
+
+    private fun printOutput() {
+        val unName = if (numberDouble != 1.0) unitOfMeasure.plural else unitOfMeasure.single
+        val outputNum = numberDouble * unitOfMeasure.conv
+        println("$numberDouble $unName is $outputNum meter${if (outputNum != 1.0) "s" else ""}")
+    }
 }
 
-fun printOutput(unitOfMeasure: UnitOfMeasure, num: Double){
-    val unName = if (num != 1.0) unitOfMeasure.plural else unitOfMeasure.single
-    val outputNum = num * unitOfMeasure.conv
-    println("$num $unName is $outputNum meter${if (outputNum!=1.0) "s" else ""}")
-}
